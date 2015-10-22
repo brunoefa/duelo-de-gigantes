967ff96b18996164dbb208205a87f86557d392ae
Bruno Eustáquio <brunoefa@gmail.com>
2015-10-22 20:09:50 -0200

    inclusao e utilizacao da taglib jstl comunicacao entre servlet e jsp

--- src/com/senai/controle/IndexServlet.java
+++ src/com/senai/controle/IndexServlet.java
@@ -1,8 +1,8 @@
 package com.senai.controle;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,32 +12,65 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/inicio")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    
+	private Double n1, n2, resultado;
+	private String op;
+	
     public IndexServlet() {
         super();
     }
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		capturarParametros(request);
+		realizarOperacao();
+		encaminharRequisicao(request, response);
+	}
+	
+	private void capturarParametros(HttpServletRequest request) throws ServletException, IOException {
 		String string1 = request.getParameter("n1");
+		this.n1 = Double.parseDouble(string1);
+		
 		String string2 = request.getParameter("n2");
-		String op = request.getParameter("op");
-
-		Double n1 = Double.parseDouble(string1);
-		Double n2 = Double.parseDouble(string2);
+		this.n2 = Double.parseDouble(string2);
 		
-		Double resultado = 0.0;
-		
-		switch (op) {
-		case "+": resultado = n1 + n2; break;
-		case "-": resultado = n1 - n2; break;
-		case "*": resultado = n1 * n2; break;
-		case "/": resultado = n1 / n2; break;
+		this.op = request.getParameter("op");
+	}
+	
+	private void realizarOperacao() {
+		this.resultado = 0.0;
+		switch (this.op) {
+		case "+": this.resultado = this.n1 + this.n2; break;
+		case "-": this.resultado = this.n1 - this.n2; break;
+		case "*": this.resultado = this.n1 * this.n2; break;
+		case "/": this.resultado = this.n1 / this.n2; break;
 		}
-		
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.print("<H1>" + n1 + " "+ op +" " + n2 + " = " + resultado + "</H1>");
-		out.print("<a href='html/calc.html'> Voltar </a>");
-		out.close();
+	}
+	
+	private void encaminharRequisicao(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("n1", this.n1);
+		request.setAttribute("n2", this.n2);
+		request.setAttribute("resultado", this.resultado);
+		request.setAttribute("op", this.op);
+		RequestDispatcher rd = request.getRequestDispatcher("html/calc.jsp");
+		rd.forward(request, response);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
